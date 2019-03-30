d4c025d38d410dfb4a2922e193e64b39f2263451
Your Name <you@example.com>
2019-03-30 11:47:17 -0600

    Categories section is working

---
+++ SurveysApp/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,146 @@
+﻿using SurveysApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SurveysApp.Areas.Admin.Controllers
+{
+    //[Authorize]
+    public class CategoryController : Controller
+    {
+
+        private readonly Models.AppDbContext _db = new AppDbContext();
+
+        // GET: Admin/Category
+        public async Task<ActionResult> Index()
+        {
+            var categories = await _db.Category.ToListAsync();
+
+            return View(categories);
+        }
+
+        //[AllowAnonymous]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            _db.Category.Add(category);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //GET - EDIT
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var category = await _db.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(category);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            var categoryFromDb = await _db.Category.FindAsync(category.Id);
+
+            if (categoryFromDb == null)
+            {
+                return HttpNotFound();
+            }
+
+            categoryFromDb.Name = category.Name;
+
+            //_db.Update(category);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET - DELETE
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var category = await _db.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(category);
+        }
+
+        // DELETE - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id)
+        {
+            Category category = await _db.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+
+            _db.Category.Remove(category);
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET - DETAILS
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var category = await _db.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(category);
+        }
+    }
+}
---
+++ SurveysApp/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace SurveysApp.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(): base("name=AppDbContext")
+        {
+
+        }
+
+        public virtual DbSet<Category> Category { get; set; }
+    }
+}
